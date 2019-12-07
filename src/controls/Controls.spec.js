eb0ad7d1de6907b6d2732fc790676fc153bb4e8e
CryptoN999 <nicholasgallucci4@gmail.com>
2019-12-06 17:20:54 -0800

    finished all tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,50 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Controls from './Controls';
+
+test('control renders correctly.', () => {
+    expect(render(<Controls />)).toMatchSnapshot();
+});
+
+
+test('gate can be toggled closed on click.', () => {
+    const toggleToClose = jest.fn();
+    const { getByText } = render(<Controls toggleClosed={toggleToClose} closed={false} />);
+
+    const closeButton = getByText(/close/i);
+    fireEvent.click(closeButton);
+    expect(toggleToClose).toHaveBeenCalled();
+})
+
+
+test('gate cannot be closed when locked.', () => {
+    const toggleToClose = jest.fn();
+    const { getByText } = render(<Controls toggleClosed={toggleToClose} closed={false} locked={true} />)
+
+    const closeButton = getByText(/close/i);
+    fireEvent.click(closeButton);
+    expect(toggleToClose).not.toHaveBeenCalled();
+})
+
+
+test('gate cannot be opened when locked.', () => {
+    const toggleToOpen = jest.fn();
+    const { getByText } = render(<Controls toggleClosed={toggleToOpen} closed={false} locked={true} />);
+
+    const openButton = getByText(/close/i);
+    fireEvent.click(openButton);
+    expect(toggleToOpen).not.toHaveBeenCalled();
+})
+
+
+test('gate can be toggled open on click.', () => {
+    const toggleToOpen = jest.fn();
+    const { getByText } = render(<Controls toggleClosed={toggleToOpen} closed={true} />);
+
+    const openButton = getByText(/open/i);
+    fireEvent.click(openButton);
+    expect(toggleToOpen).toHaveBeenCalled();
+})
+
+//cant think of anymore currently.
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('dashboard renders correctly.', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+});
+
+
+test("gate default is unlocked and open", () => {
+    const dashboard = render(<Dashboard/>);
+    const lockBtn = dashboard.getByText(/locK gate/i);
+    const openBtn = dashboard.getByText(/close gate/i);
+    expect(lockBtn.disabled).toBe(true);
+    expect(openBtn.disabled).toBe(false);
+});
+
+
+//tried like 5 other tests here and all failed...
